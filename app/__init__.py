029f6bac7ef8f26d46868989ccd2827e78832fba
unknown <nyokjohnson@gmail.com>
2025-04-16 13:08:51 +0300

    Made some changes

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@ def create_app(config_class=Config):
     """A function to and configure flask app"""
     app = Flask(__name__)
     app.config.from_object(config_class)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or "sqlite:///default.db"
 
     #Initialize extensions with app
     db.init_app(app)

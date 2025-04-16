9e5557e27be32710abded7455e210bea392831dc
unknown <nyokjohnson@gmail.com>
2025-04-16 04:43:06 +0300

    fix: use DATABASE_URL for SQLAlchemy config

--- app/config.py
+++ app/config.py
@@ -4,14 +4,14 @@ from datetime import timedelta
 
 load_dotenv()
 
-
 class Config:
     """Base configuration"""
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    _db_url = os.getenv("DATABASE_URL")
+    if _db_url and _db_url.startswith("postgres://"):
+        _db_url = _db_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = _db_url
     SECRET_KEY = os.getenv('SECRET_KEY')
-    DEBUG = os.getenv('DEBUG', 'FALSE').lower()== "true"
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    DEBUG = os.getenv('DEBUG', 'FALSE').lower() == "true"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
-    

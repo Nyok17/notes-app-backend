52439dd0a85ea0e791ba8b54c2d3ed285ff34d71
unknown <nyokjohnson@gmail.com>
2025-04-16 04:49:43 +0300

    Fix: Add SQLALCHEMY_DATABASE_URI from environment1

--- app/config.py
+++ app/config.py
@@ -10,8 +10,12 @@ class Config:
     if _db_url and _db_url.startswith("postgres://"):
         _db_url = _db_url.replace("postgres://", "postgresql://", 1)
 
-    SQLALCHEMY_DATABASE_URI = _db_url
+    SQLALCHEMY_DATABASE_URI = _db_url or 'sqlite:///default.db'
     SECRET_KEY = os.getenv('SECRET_KEY')
+
+    if not SECRET_KEY:
+        raise ValueError("SECRET_KEY is not set in the environment.")
+
     DEBUG = os.getenv('DEBUG', 'FALSE').lower() == "true"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
